fc1d0c2c882a3c3dc3e213a4be272ebf318219c1
Untoldwind <landru@untoldwind.net>
2012-06-10 20:24:43 +0200

    Add some debug

--- server/app/controllers/Dashboard.scala
+++ server/app/controllers/Dashboard.scala
@@ -2,14 +2,33 @@ package controllers
 
 import play.api.mvc.Controller
 import models.ConnectionTable
+import collection.mutable.ArrayBuffer
+import java.io.File
 
 object Dashboard extends Controller with Secured {
 
   def index = Authenticated {
     implicit request =>
-      if ( request.user.canAdmin)
-        Ok(views.html.dashboard.index(ConnectionTable.findAll))
+      val files = new ArrayBuffer[String]
+      val root = new File(".")
+      files += root.getAbsolutePath
+      root.listFiles().foreach {
+        f =>
+          files += f.getAbsolutePath
+          if ( f.isDirectory ) {
+            f.listFiles().foreach { sf =>
+              files += sf.getAbsolutePath
+              if ( sf.isDirectory ) {
+                sf.listFiles().foreach { ssf =>
+                  files += ssf.getAbsolutePath
+                }
+              }
+            }
+          }
+      }
+      if (request.user.canAdmin)
+        Ok(views.html.dashboard.index(ConnectionTable.findAll, files))
       else
-        Ok(views.html.dashboard.index(ConnectionTable.findForUser(request.user)))
+        Ok(views.html.dashboard.index(ConnectionTable.findForUser(request.user), files))
   }
 }
